277204681ddda2ffe3bc21c24d2376e1bb50fcf4
Shafik <shafik_damji@yahoo.ca>
2017-10-29 13:05:18 -0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -32,11 +32,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function(){
-
+    messageTextBox.val('');
   });
 });
 
@@ -46,15 +48,18 @@ locationButton.on('click', function() {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending ...');
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
 
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
-    console.log('I am here 3');
 
   }, function() {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location.');
 
   });
